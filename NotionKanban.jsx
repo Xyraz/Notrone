3f4bf4424b7a433add6b5182cc21658f819cbb3a
xyrix <xiryx@outlook.com>
2025-05-07 08:01:49 +0100

    defining different states of the project tasks could have

--- NotionKanban.jsx
+++ NotionKanban.jsx
@@ -9,3 +9,38 @@ const Board = () => {
 
      return <div className="flex h-full w-full gap-3 overflow-scroll p-12"></div>
 }
+
+const Column = ({title,headingColor,column,cards,
+    setCards}) => {
+        return <div className ="flex h-full w-full gap-3 overflow-scroll p-12">
+            <Column
+            title="Backlog"
+            column="backlog"
+            headingColor="text-neutral-500"
+            cards={cards}
+            setCards={setCards}
+            />
+            <Column
+            title="TODO"
+            column="todo"
+            headingColor="text-yellow-200"
+            cards={cards}
+            setCards={setCards}
+            />
+            <Column
+            title="In progress"
+            column="doing"
+            headingColor="text-blue-200"
+            cards={cards}
+            setCards={setCards}
+            />
+            <Column
+            title="Complete"
+            column="complete"
+            headingColor="text-emerald-200"
+            cards={cards}
+            setCards={setCards}
+            />
+        </div>
+
+}
