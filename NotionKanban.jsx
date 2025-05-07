4b760bcadcadd1f621a6cb77f2b9f00bbc1271ec
xyrix <xiryx@outlook.com>
2025-05-07 07:51:55 +0100

    creat component which will be used to make it possible to change the cards from position(progress in project)

--- NotionKanban.jsx
+++ NotionKanban.jsx
@@ -4,3 +4,8 @@ export const NotionKanban =   () => {
     return <div className="h-screen w-full bg-neutral-900 text-neutral-50"></div>
 }
 
+const Board = () => {
+     const [cards, setCards] = useState([]);
+
+     return <div className="flex h-full w-full gap-3 overflow-scroll p-12"></div>
+}
