2fbc5902eeefaa0d447b7e9263cfc23ca404b240
xyrix <xiryx@outlook.com>
2025-05-12 10:33:24 +0100

    adding some styling

--- NotionKanban.jsx
+++ NotionKanban.jsx
@@ -1,46 +1,51 @@
 import React from "react";
 
-export const NotionKanban =   () => {
+export const NotionKanban = () => {
     return <div className="h-screen w-full bg-neutral-900 text-neutral-50"></div>
 }
 
 const Board = () => {
-     const [cards, setCards] = useState([]);
+    const [cards, setCards] = useState([]);
 
-     return <div className="flex h-full w-full gap-3 overflow-scroll p-12"></div>
+    return <div className="flex h-full w-full gap-3 overflow-scroll p-12"></div>
 }
 
-const Column = ({title,headingColor,column,cards,
-    setCards}) => {
-        return <div className ="flex h-full w-full gap-3 overflow-scroll p-12">
+const Column = ({ title, headingColor, column, cards,
+    setCards }) => {
+    return (
+        <div className="flex h-full w-full gap-3 overflow-scroll p-12">
             <Column
-            title="Backlog"
-            column="backlog"
-            headingColor="text-neutral-500"
-            cards={cards}
-            setCards={setCards}
+                title="Backlog"
+                column="backlog"
+                headingColor="text-neutral-500"
+                cards={cards}
+                setCards={setCards}
             />
             <Column
-            title="TODO"
-            column="todo"
-            headingColor="text-yellow-200"
-            cards={cards}
-            setCards={setCards}
+                title="TODO"
+                column="todo"
+                headingColor="text-yellow-200"
+                cards={cards}
+                setCards={setCards}
             />
             <Column
-            title="In progress"
-            column="doing"
-            headingColor="text-blue-200"
-            cards={cards}
-            setCards={setCards}
+                title="In progress"
+                column="doing"
+                headingColor="text-blue-200"
+                cards={cards}
+                setCards={setCards}
             />
             <Column
-            title="Complete"
-            column="complete"
-            headingColor="text-emerald-200"
-            cards={cards}
-            setCards={setCards}
+                title="Complete"
+                column="complete"
+                headingColor="text-emerald-200"
+                cards={cards}
+                setCards={setCards}
             />
         </div>
+    )
+};
 
+const Column = ({ title,headingColor, column, cards, setCards}) => {
+    return <div className="w-56 shrink-0"></div>
 }
